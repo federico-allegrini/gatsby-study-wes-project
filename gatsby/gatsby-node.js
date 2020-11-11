5210c5b018d567159a975bd5eddb01bdb3709d58
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-11-11 11:38:22 +0100

    Dynamically creating toppings pages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -29,10 +29,42 @@ async function turnPizzasIntoPages({ graphql, actions }) {
   });
 }
 
+async function turnToppingsIntoPages({ graphql, actions }) {
+  // 1. Get a template
+  const toppingTemplate = path.resolve('./src/pages/pizzas.js');
+  // 2. Query all toppings
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+        }
+      }
+    }
+  `);
+  // 3. Loop over each and create a page for that topping
+  // 4. Pass topping data to pizza.js
+  data.toppings.nodes.forEach((topping) => {
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingTemplate,
+      context: {
+        name: topping.name,
+        toppingRegex: `/${topping.name}/i`,
+      },
+    });
+  });
+}
+
 export async function createPages(params) {
   // Create pages dinamically
-  // 1. Pizzas
-  await turnPizzasIntoPages(params);
-  // 2. Toppings
-  // 3. Slicemasters
+  //  Wait for all promises to be resolved before finishing this function
+  await Promise.All([
+    // 1. Pizzas
+    turnPizzasIntoPages(params),
+    // 2. Toppings
+    turnToppingsIntoPages(params),
+    // 3. Slicemasters
+  ]);
 }
