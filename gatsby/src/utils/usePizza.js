16d381c5fb83d579a03d375e13945935dfacfde2
Federico Allegrini <federico.allegrini.dev@gmail.com>
2020-11-29 13:48:40 +0100

    Modifying our custom hook to send the order data

--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,7 +1,10 @@
 import { useContext, useState } from 'react';
 import OrderContext from '../components/OrderContext';
+import attachNamesAndPrices from './attachNamesAndPrices';
+import calculateOrderTotal from './calculateOrderTotal';
+import formatMoney from './formatMoney';
 
-export default function usePizza({ pizzas, inputs }) {
+export default function usePizza({ pizzas, values }) {
   // 1. Create some state to hold our order
 
   // We got rid of this line because we move useState up to the provider
@@ -9,6 +12,9 @@ export default function usePizza({ pizzas, inputs }) {
 
   // Now we access both our state and our updater function (setOrder) via context
   const [order, setOrder] = useContext(OrderContext);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   // 2. Make a function add things to order
   function addToOrder(orderedPizza) {
@@ -23,12 +29,51 @@ export default function usePizza({ pizzas, inputs }) {
       ...order.slice(index + 1),
     ]);
   }
-  // 4. Send this data to a serverless function when they check out
-  // TODO
+
+  // This is the function that is run when someone sumbits the form
+  async function submitOrder(e) {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    setMessage(null);
+    // Gather all the data
+    const body = {
+      order: attachNamesAndPrices(order, pizzas),
+      total: formatMoney(calculateOrderTotal(order, pizzas)),
+      name: values.name,
+      email: values.email,
+    };
+    console.log(body);
+    // 4. Send this data to a serverless function when they check out
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const text = JSON.parse(await res.text());
+    // Check if everything worked
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false); // Turn off loading
+      setError(text.message);
+    } else {
+      // It worked!
+      setLoading(false);
+      setMessage('Success! Come on down for you pizza');
+    }
+  }
 
   return {
     order,
     addToOrder,
     removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
   };
 }
